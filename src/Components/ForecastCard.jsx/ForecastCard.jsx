f3f9e204f922a7f9e13626ae99e33dbcb83c062c
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-07-10 22:52:31 +0600

    weather temperature full complete

--- src/Components/ForecastCard.jsx/ForecastCard.jsx
+++ src/Components/ForecastCard.jsx/ForecastCard.jsx
@@ -5,17 +5,18 @@ const ForecastCard = ({allUpcommingDay}) => {
     console.log(allUpcommingDay)
     
     const{date , day} = allUpcommingDay
+
     
     return (
         <div className="card w-full bg-black bg-opacity-40 shadow-xl text-gray-200">
-            <figure><img src="https://openweathermap.org/img/wn/02d@2x.png" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{moment(date).format('dddd')}</h2>
+            <figure><img title={day?.condition?.text} src={day?.condition.icon} alt="Shoes" /></figure>
+            <div>
+                <h2 className="text-center md:text-2xl text-small">{moment(date).format('dddd')}</h2>
                 {/* <p>{parseInt(day?.maxtemp_c)}</p> */}
 
-                <h2 className="mt-2 text-3xl text-white"> <span>{parseInt(day?.maxtemp_c)}</span><sup>°C</sup></h2>
+                <h2 className="mt-2 md:text-2xl p-3 md:ms-3 text-lg text-white">  <span>{parseInt(day?.maxtemp_c)}</span><sup>°C</sup></h2>
 
-               
+        
             </div>
         </div>
     );
--- src/Page/Weather/Weather.jsx
+++ src/Page/Weather/Weather.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import './Weather.css'
 import { useEffect } from 'react';
-import ForecastCard from '../../Components/ForecastCard/ForecastCard';
 import moment from 'moment';
+import ForecastCard from '../../Components/ForecastCard.jsx/ForecastCard';
 
 const Weather = () => {
 
     const [weather, setWeather] = useState({})
     const [searchValue, SetSearchValue] = useState("london")
+    const [sortOrder, setSortOrder] = useState('asc');
 
     useEffect(() => {
         fetch(`http://api.weatherapi.com/v1/forecast.json?key=56a94bcd21be43668e0110744231007 &q=${searchValue}&days=7&aqi=no&alerts=no`)
@@ -22,8 +23,6 @@ const Weather = () => {
 
     const { location, current, forecast } = weather
     console.log(weather)
-    // console.log(location , weather?.location?.name)
-
 
 
 
@@ -37,6 +36,20 @@ const Weather = () => {
     }
 
 
+
+    const handleSort = () => {
+        if (sortOrder === 'asc') {
+
+            forecast?.forecastday.sort((a, b) => a.day.maxtemp_c - b.day.maxtemp_c);
+            setSortOrder('desc');
+
+        } else {
+
+            forecast?.forecastday.sort((a, b) => b.day.maxtemp_c - a.day.maxtemp_c);
+
+            setSortOrder('asc');
+        }
+    };
 
 
     return (
@@ -55,22 +68,33 @@ const Weather = () => {
 
 
 
-                    <div className="text-center">
-                        <img className="mx-auto" src="https://openweathermap.org/img/wn/02d@2x.png" alt="" />
-                        <h1 className="text-5xl text-white font-semibold">{location?.name}</h1>
-                        <h2 className="mt-2 text-3xl text-white"> <span>{current?.temp_c}</span><sup>°C</sup></h2>
-                        <h1 className="text-2xl text-white">{moment().format('dddd')} </h1>
+                    {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto"> */}
+                    {
+                        weather?.location && <div className='mx-auto text-center pt-5'>
+
+                            <img title={current?.condition?.text} className="mx-auto" src={current?.condition?.icon} alt="" />
+                            <h1 className="text-5xl text-white font-semibold">{location?.name}</h1>
+                            <h2 className="mt-2 text-3xl text-white"> <span>{current?.temp_c}</span><sup>°C</sup></h2>
+                            <h1 className="text-2xl text-white">{moment(current?.last_updated).format('dddd')} </h1>
+                            <p className="mt-2 text-lg text-white"> <span className='font-semibold'>  Wind :</span>  <span>{parseInt(current?.wind_kph)}</span><span>Km/h</span></p>
+                            <p className=" text-lg text-white"> <span className='font-semibold'>humidity :</span>  <span>{current?.humidity}</span></p>
+
+                        </div>
+                    }
+
+                    <div className='text-center py-5'>
+                        {/* <button className='btn btn-xs'>Week Highest Tempature</button> */}
+                        <button onClick={handleSort} className='btn btn-xs'> {sortOrder === 'asc' ? 'Lowest temperature in Week' : 'highest temperature in Week'}</button>
                     </div>
 
 
 
-
-                        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-7 gap-6 px-10 mx-auto'>
-                            {
-                                forecast?.forecastday.map(allUpcommingDay => <ForecastCard key={allUpcommingDay.date} allUpcommingDay={allUpcommingDay}></ForecastCard>
-                                )
-                            }
-                        </div>
+                    <div className='grid grid-cols-4 pb-16 md:grid-cols-3 lg:grid-cols-7 gap-6 px-10 mx-auto'>
+                        {
+                            forecast?.forecastday.map(allUpcommingDay => <ForecastCard key={allUpcommingDay.date} allUpcommingDay={allUpcommingDay}></ForecastCard>
+                            )
+                        }
+                    </div>
 
 
 
